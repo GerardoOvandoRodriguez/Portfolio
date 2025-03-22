18cc4400dd13262bc7d17430d22e9e41e475aa66
Gerardo Ovando Rodriguez <gerardo.o.rodriguez@hotmail.com>
2025-03-22 17:13:21 -0600

    Adding: Animations in NavBar - Cards

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -5,9 +5,6 @@ for(let i = 0; i < navbarText.length; i++){
     nave.push(navbarText[i]) ;
 }
 
-console.log(navbarText[1]);
-console.log(nave);
-
 nave.forEach(e =>{
 
     e.addEventListener('mouseenter',()=>{
@@ -23,61 +20,39 @@ nave.forEach(e =>{
 // Cards animation
 
 const card = document.querySelectorAll('.Card');
-console.log(card);
 
 const cardArray = [];
 
 for(let i=0;i < card.length;i++){
     cardArray.push(card[i]); 
 };
-console.log(cardArray);
 
 cardArray.forEach(e =>{
     
     e.addEventListener('mouseenter',()=>{
-       e.style.height = "380px" 
+       e.style.scale = "110%" 
     });
     e.addEventListener('mouseout',()=>{
-        e.style.height = "350px" 
+        e.style.scale = "100%" 
      });
-    e.addEventListener('mouseenter',()=>{
-        e.style.width = "31%" 
-    });
-    e.addEventListener('mouseout',()=>{
-        e.style.width = "30%" 
-    });
-    
 });
 
-//Words animation
-const words = document.querySelector('.NameAnimation');
-console.log(words.textContent.split("", words.textContent.length));
+const button = document.querySelector('.Button');
 
-const nameAnimation = words.textContent;
+console.log(button);
 
-
-
-console.log(nameAnimation.length);
-
-const wordsArray = words.textContent.split("",words.textContent.length);
-const wordsArray2 = [words.textContent.split("",words.textContent.length)]
-
-console.log(wordsArray2)
-
-for(let i=0;i < nameAnimation.length;i++){
-    
-}
-
-
-function animation(){
-    
-    wordsArray.pop();
-    words.textContent = wordsArray;
-}
+button.addEventListener('mouseenter',()=>{
+    button.style.scale = "110%";
+})
+button.addEventListener('mouseout',()=>{
+    button.style.scale = "100%";
+})
 
 
 
-setInterval(animation,150);
+
+
+
 
 
 
